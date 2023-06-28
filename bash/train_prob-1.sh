9bc654147143f3a69adb08b6fc78d0d9e03fab35
phamthaihoangtung <phamthaihoangtung@gmail.com>
2023-06-28 03:53:43 +0000

    add bash file

---
+++ bash/train_prob-1.sh
@@ -0,0 +1,4 @@
+python3 src/model_trainer.py \
+        --phase-id phase-1 \
+        --prob-id prob-1 \
+        --time_tuning 0
---
+++ bash/tuning_prob-2.sh
@@ -0,0 +1,4 @@
+python3 src/model_trainer.py \
+        --phase-id phase-1 \
+        --prob-id prob-2 \
+        --time_tuning 100
