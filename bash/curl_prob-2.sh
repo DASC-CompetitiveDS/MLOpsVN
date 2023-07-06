2097a8ade93f33a7a7e7ecdd313bfcbf44ff70e3
phamthaihoangtung <phamthaihoangtung@gmail.com>
2023-07-06 10:33:00 +0000

    update khiempm2 to main

--- bash/curl_prob-2.sh
+++ bash/curl_prob-2.sh
@@ -1,3 +1,3 @@
-curl -X POST http://localhost:8000/phase-1/prob-2/predict \
+curl -X POST http://localhost:8000/phase-2/prob-2/predict \
         -H "Content-Type: application/json" \
-        -d @data/curl/phase-1/prob-2/payload-1.json
+        -d @data/curl/phase-2/prob-2/payload-1.json
