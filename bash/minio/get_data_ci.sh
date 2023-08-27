440d4a5e119f803811903feb46f779e913e43a6f
phamthaihoangtung <phamthaihoangtung@gmail.com>
2023-08-27 04:22:40 +0000

    fix ci and get capture 11-13

--- bash/minio/get_data_ci.sh
+++ bash/minio/get_data_ci.sh
@@ -1,2 +1,2 @@
 python3 src/storage_utils/folder_getter.py --minio_server $1 --include_pattern $2 --exclude_pattern $3 --dst_path $4
-python3 src/storage_utils/get_captured_data.py --minio_server $1 --src_path data/captured_data/phase-3 --captured_version $5 --drop_exist True
+python3 src/storage_utils/get_captured_data.py --minio_server $1 --src_path data/captured_data/phase-3 --captured_version $5 --drop_exist False
