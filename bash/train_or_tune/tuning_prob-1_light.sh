46a96c80c3623ed8456d44b1284b04bd2a0de709
phamthaihoangtung <phamthaihoangtung@gmail.com>
2023-09-03 06:01:02 +0000

    ci training

--- bash/train_or_tune/tuning_prob-1_light.sh
+++ bash/train_or_tune/tuning_prob-1_light.sh
@@ -1,9 +1,10 @@
+TIME_TUNING="${1:-1200}"
+
 python3 src/model_trainer.py \
         --phase-id phase-3 \
         --prob-id prob-1 \
-        --time_tuning 1200 \
+        --time_tuning $TIME_TUNING \
         --cross_validation True \
-        --specific_handle True \
         --early_stopping_rounds 50 \
-        --learning_rate 0.5 \ 
-        # --model_name cv_lr-0.2 \
+        --learning_rate 0.5 \
+        --model_name test_ci
